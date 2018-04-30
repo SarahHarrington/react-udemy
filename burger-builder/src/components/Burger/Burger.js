31af25ceaf9bf7538be1812085fe1e3cf2774475
Sarah Harrington <sarah.soberg@gmail.com>
2018-04-30 16:21:59 -0500

    updated per lecture

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,13 +4,21 @@ import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
 
-  const transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
+  let transformedIngredients = Object.keys(props.ingredients) //this creates an array of string keys
+    .map(igKey => { //ingredient key - the array key
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredients key={igKey + i} type={igKey} />
       });
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []) ;
 
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }
+  
+  console.log(transformedIngredients)
   return (
     <div className={classes.Burger}>
       <BurgerIngredients type="bread-top" />
